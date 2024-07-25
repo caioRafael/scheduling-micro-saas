b4513fb2834f3fb8ec41abbbdbedf8b8f455b114
caioRafael <caiorafaelrg@gmail.com>
2024-07-25 13:12:50 -0300

    melhoria visual na pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="w-screen min-h-screen flex flex-col items-center justify-center">
+    <main className="w-screen min-h-screen flex flex-col items-center justify-center gap-2">
       <h1>HealthPlaning</h1>
-      <Link href="/auth">Login</Link>
-      <Link href="/app">Aplicação</Link>
+      <Link href="/auth" className="p-3 rounded-md bg-primary text-secondary">
+        Login
+      </Link>
+      <Link href="/app" className="p-3 rounded-md bg-primary text-secondary">
+        Aplicação
+      </Link>
     </main>
   )
 }
